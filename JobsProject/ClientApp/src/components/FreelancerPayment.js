054558104904634325ad7c16157ebdeddfaa37bc
stefan22117 <65757609+stefan22117@users.noreply.github.com>
2022-01-08 20:10:00 +0100

    finish

--- JobsProject/ClientApp/src/components/FreelancerPayment.js
+++ JobsProject/ClientApp/src/components/FreelancerPayment.js
@@ -1,100 +1,202 @@
-import { Button, Grid } from '@material-ui/core'
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux';
-import { useParams,useHistory } from 'react-router';
+import { Button, Grid, Paper } from "@material-ui/core";
+import { Person, AttachMoney } from "@material-ui/icons";
+import { withStyles } from "@material-ui/core/styles";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { useParams, useHistory } from "react-router";
 import * as jobActions from "../redux/actions/jobActions";
 import * as userActions from "../redux/actions/userActions";
-import {Link} from 'react-router-dom'
-import { toast } from 'react-toastify';
-const FreelancerPayment = ({loggedUser,bidFreelancerJob, ...props}) => {
-    const history = useHistory();
-    const params = useParams();
-    useEffect(() => {
-        props.getBidFreelancerJob(params.id)
-    }, []);
-    const total = bidFreelancerJob?.activeBid?.amount / bidFreelancerJob?.job?.valute?.toDinars;
- 
+import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
-    const formatTotal = (total, decimals=2)=>{
-        return (total+"").indexOf(".") != -1?
-        (total+"").slice(
-          0,(total+"").indexOf(".")+decimals+1
-          ):
-        total;
+const styles = (theme) => ({
+  text: {
+    color: "#00203f",
+    textAlign: "center",
+  },
+  button:{
+      color:'#adefd1',
+      backgroundColor:'#00203f',
+      '&:hover':{
+        color:'#adefd1',
+        backgroundColor:'#00203f',
+      }
+  }
+});
+
+const FreelancerPayment = ({
+  loggedUser,
+  bidFreelancerJob,
+  classes,
+  ...props
+}) => {
+  const history = useHistory();
+  const params = useParams();
+  useEffect(() => {
+    props.getBidFreelancerJob(params.id);
+  }, []);
+  const total =
+    bidFreelancerJob?.activeBid?.amount /
+    bidFreelancerJob?.job?.valute?.toDinars;
+
+  const formatTotal = (total, decimals = 2) => {
+    return (total + "").indexOf(".") != -1
+      ? (total + "").slice(0, (total + "").indexOf(".") + decimals + 1)
+      : total;
+  };
+
+  const payFreelancerHandler = async () => {
+    if (bidFreelancerJob?.job?.finished) {
+      toast.error("You have already paid a freelancer for this job");
+      return;
     }
 
-    const payFreelancerHandler = async () =>{
-
-        if(bidFreelancerJob?.job?.finished){
-            return toast("You have already paid a freelancer for this job");
-        }
-
-        if(loggedUser.total- total<0){
-           return toast("You need: "+ formatTotal(total - loggedUser.total) +" " +loggedUser?.valute?.namePlural.toLowerCase() + " more")
-        }
-            await props.completeJob(params.id);
-            toast("You paid the user " + bidFreelancerJob.user.name+" " +formatTotal(total)+ loggedUser?.valute?.namePlural.toLowerCase());
-            history.push('/jobs/'+params.id);
-            await props.getLoggedUser();
-        
+    if (loggedUser.total - total < 0) {
+      toast.error(
+        "You need: " +
+          formatTotal(total - loggedUser.total) +
+          " " +
+          loggedUser?.valute?.namePlural.toLowerCase() +
+          " more"
+      );
+      return;
     }
-    return (
-        <div>
-            <h1>{bidFreelancerJob?.job?.title}</h1>
-            <Grid container>
-                <Grid item md={4}>
-                <p>Your balance: {loggedUser.total}{loggedUser?.valute?.label}</p>
-                </Grid>
-                <Grid item md={4}>
-                    <p>Payment: {bidFreelancerJob?.job?.paymentType}</p>
-                    <p>Price: {bidFreelancerJob?.activeBid?.amount} {bidFreelancerJob?.job?.valute?.label}</p>
-                  {
-                     loggedUser?.valute?.id != bidFreelancerJob?.job?.valute?.id &&
-                     
-                     <p>Converted: -{ 
-                        formatTotal(total)
-                        } {loggedUser?.valute?.label} </p>
-                  }
+    await props.completeJob(params.id);
+    toast.success(
+      "You paid the user " +
+        bidFreelancerJob.user.name +
+        " " +
+        formatTotal(total) +
+        loggedUser?.valute?.namePlural.toLowerCase()
+    );
+    history.push("/jobs/" + params.id);
+    await props.getLoggedUser();
+  };
+  return (
+    <Paper
+      elevation={2}
+      style={{
+        backgroundColor: "#adefd1",
+        marginBottom: 20,
 
-<p>Your Bilance: { 
-                        (loggedUser.total- total+"").indexOf(".") != -1?
-                        (loggedUser.total- total+"").slice(
-                          0,(loggedUser.total - total+"").indexOf(".")+3
-                          ):
-                          loggedUser.total- total
-                        } {loggedUser?.valute?.label} </p>
+        display: "flex",
+        padding: 5,
+      }}
+    >
+      <Grid
+        containter
+        style={{
+          width: "100%",
+        }}
+      >
+        <Grid item md={12}>
+          <h1 className={classes.text}>{bidFreelancerJob?.job?.title}</h1>
+        </Grid>
 
+        <Grid container item md={12}>
+          <Grid
+            item
+            container
+            md={4}
+            sm={6}
+            xs={12}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <h3 className={classes.text}>
+              Your balance: {loggedUser.total}
+              {loggedUser?.valute?.label}
+            </h3>
+          </Grid>
+          <Grid
+            item
+            md={4}
+            sm={6}
+            xs={12}
+            style={{
+              color: "#00203f",
+            }}
+          >
+            <p className={classes.text}>
+              Payment: {bidFreelancerJob?.job?.paymentType}
+            </p>
+            <p className={classes.text}>
+              Price: {bidFreelancerJob?.activeBid?.amount}{" "}
+              {bidFreelancerJob?.job?.valute?.label}
+            </p>
+            {loggedUser?.valute?.id != bidFreelancerJob?.job?.valute?.id && (
+              <p className={classes.text}>
+                Converted: {formatTotal(total)} {loggedUser?.valute?.label}{" "}
+              </p>
+            )}
 
-
-                </Grid>
-                <Grid item md={4}>
-                    <Link to={"/freelancers/"+bidFreelancerJob?.user?.id}>{bidFreelancerJob?.user?.name}</Link>
-                <Button
-                 variant='contained'
-                  color='primary' 
-                  disabled={bidFreelancerJob?.job?.finished}
-                  onClick={()=>payFreelancerHandler(bidFreelancerJob?.job?.id)}>Pay</Button>
-                </Grid>
-
+            <p className={classes.text}>
+              Your Bilance:{" "}
+              {(loggedUser.total - total + "").indexOf(".") != -1
+                ? (loggedUser.total - total + "").slice(
+                    0,
+                    (loggedUser.total - total + "").indexOf(".") + 3
+                  )
+                : loggedUser.total - total}{" "}
+              {loggedUser?.valute?.label}{" "}
+            </p>
+          </Grid>
+          <Grid item container md={4} sm={12} xs={12}>
+            <Grid item
+            container 
+            justifyContent="center" 
+            alignItems="center"
+            md={12}>
+              <Button
+              className={classes.button}
+              style={{
+                  marginBottom:5
+              }}
+                variant="contained"
+                color="primary"
+                onClick={() =>
+                  history.push("/freelancers/" + bidFreelancerJob?.user?.id)
+                }
+              >
+                <Person /> {bidFreelancerJob?.user?.name}
+              </Button>
             </Grid>
-        </div>
-    )
-}
+            <Grid item 
+            container 
+            justifyContent="center" 
+            alignItems="center"
+            md={12} >
+              <Button
+              className={classes.button}
+                variant="contained"
+                color="primary"
+                disabled={bidFreelancerJob?.job?.finished}
+                onClick={() => payFreelancerHandler(bidFreelancerJob?.job?.id)}
+              >
+                Pay{" "}<AttachMoney />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 const mapStateToProps = (state) => ({
-    loggedUser: state.usersReducer.user,
-    technologiesList: state.technologiesReducer.technologiesByJobId,
-    bidsList: state.bidsReducer.list,
-    bidFreelancerJob: state.jobsReducer.singleJob,
-  });
-  
-  const mapDispatchToProps = {
-    getSingleJob: jobActions.findById,
-    getBidFreelancerJob: jobActions.findByCompletedJobId,
-    completeJob: jobActions.completeJob,
-    getLoggedUser: userActions.getLoggedUser,
-    // technologiesByJobId: technologyActions.fetchByJobId,
-    // bidsByJobId: bidActions.fetchByJobId,
-  };
-  export default connect(mapStateToProps, mapDispatchToProps)(FreelancerPayment);
+  loggedUser: state.usersReducer.user,
+  technologiesList: state.technologiesReducer.technologiesByJobId,
+  bidsList: state.bidsReducer.list,
+  bidFreelancerJob: state.jobsReducer.singleJob,
+});
 
+const mapDispatchToProps = {
+  getSingleJob: jobActions.findById,
+  getBidFreelancerJob: jobActions.findByCompletedJobId,
+  completeJob: jobActions.completeJob,
+  getLoggedUser: userActions.getLoggedUser,
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(FreelancerPayment));
